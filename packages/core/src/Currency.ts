ec4ba12fb075cdd9a426526d659a469203de3cd2
Jeremy Klein <jklein24@gmail.com>
2024-02-27 21:48:19 -0800

    Add support for the new LUD-21 spec. (#71)
    * Add support for the new LUD-21 spec.
    
    Allow specifying the payment amount in sats.
    
    * format

--- packages/core/src/Currency.ts
+++ packages/core/src/Currency.ts
@@ -22,18 +22,6 @@ export const CurrencySchema = z.object({
   multiplier: z.number(),
 
   /**
-   * Minimum amount that can be sent in this currency. This is in the smallest unit of the currency
-   * (eg. cents for USD).
-   */
-  minSendable: z.number(),
-
-  /**
-   * Maximum amount that can be sent in this currency. This is in the smallest unit of the currency
-   * (eg. cents for USD).
-   */
-  maxSendable: z.number(),
-
-  /**
    * The number of digits after the decimal point for display on the sender side, and to add clarity
    * around what the "smallest unit" of the currency is. For example, in USD, by convention, there are 2 digits for
    * cents - $5.95. In this case, `decimals` would be 2. Note that the multiplier is still always in the smallest
@@ -43,6 +31,24 @@ export const CurrencySchema = z.object({
    * For details on edge cases and examples, see https://github.com/uma-universal-money-address/protocol/blob/main/umad-04-lnurlp-response.md.
    */
   decimals: z.number(),
+
+  /**
+   * The inclusion of a convertible field implies the receiving VASP can quote and guarantee a price
+   * for a given currency.
+   */
+  convertible: z.object({
+    /**
+     * Minimum amount that can be sent in this currency. This is in the smallest unit of the currency
+     * (eg. cents for USD).
+     */
+    min: z.number(),
+
+    /**
+     * Maximum amount that can be sent in this currency. This is in the smallest unit of the currency
+     * (eg. cents for USD).
+     */
+    max: z.number(),
+  }),
 });
 
 export type Currency = z.infer<typeof CurrencySchema>;
--- packages/core/src/protocol.ts
+++ packages/core/src/protocol.ts
@@ -98,20 +98,108 @@ export function parseLnurlpResponse(jsonStr: string): LnurlpResponse {
 }
 
 /**
- * PayRequest is the request sent by the sender to the receiver to retrieve an invoice.
+ * The schema of the request sent by the sender to the receiver to retrieve an invoice.
  */
 export const PayRequestSchema = z.object({
   /** The 3-character currency code that the receiver will receive for this payment. */
-  currency: z.string(),
-  /** The amount that the receiver will receive for this payment in the smallest unit of the specified currency (i.e. cents for USD). */
-  amount: z.number(),
+  convert: z.string(),
+  /**
+   * An amount (int64) followed optionally by a "." and the sending currency code. For example: "100.USD" would send
+   * an amount equivalent to $1 USD. Note that the amount is specified in the smallest unit of the specified
+   * currency (eg. cents for USD). Omitting the currency code will default to specifying the amount in millisats.
+   */
+  amount: z.coerce.string(),
   /** The data that the sender will send to the receiver to identify themselves. See LUD-18. */
   payerData: PayerDataSchema,
   /** The fields requested about the payee by the sending vasp, if any. */
   payeeData: optionalIgnoringNull(CounterPartyDataOptionsSchema),
 });
 
-export type PayRequest = z.infer<typeof PayRequestSchema>;
+/**
+ * A class which wraps the `PayRequestSchema` and provides a more convenient interface for
+ * creating and parsing PayRequests.
+ *
+ * NOTE: The `fromJson` and `toJson` methods are used to convert to and from JSON strings.
+ * This is necessary because `JSON.stringify` will not include the correct field names.
+ */
+export class PayRequest {
+  receivingCurrencyCode: string;
+  sendingAmountCurrencyCode: string;
+  amount: number;
+  payerData: z.infer<typeof PayerDataSchema>;
+  requestedPayeeData: CounterPartyDataOptions | undefined;
+
+  constructor(
+    receivingCurrencyCode: string,
+    sendingAmountCurrencyCode: string,
+    amount: number,
+    payerData: z.infer<typeof PayerDataSchema>,
+    requestedPayeeData?: CounterPartyDataOptions,
+  ) {
+    this.receivingCurrencyCode = receivingCurrencyCode;
+    this.sendingAmountCurrencyCode = sendingAmountCurrencyCode;
+    this.amount = amount;
+    this.payerData = payerData;
+    this.requestedPayeeData = requestedPayeeData;
+  }
+
+  toJsonSchemaObject(): z.infer<typeof PayRequestSchema> {
+    return {
+      convert: this.receivingCurrencyCode,
+      amount:
+        this.sendingAmountCurrencyCode == "SAT"
+          ? this.amount.toString()
+          : `${this.amount}.${this.sendingAmountCurrencyCode}`,
+      payerData: this.payerData,
+      payeeData: this.requestedPayeeData,
+    };
+  }
+
+  /**
+   * NOTE: This MUST be used when sending the PayRequest to the receiver rather than
+   * `JSON.stringify` because the latter will not include the correct field names.
+   * @returns a JSON string representation of the PayRequest.
+   */
+  toJson(): string {
+    return JSON.stringify(this.toJsonSchemaObject());
+  }
+
+  static fromSchema(schema: z.infer<typeof PayRequestSchema>): PayRequest {
+    let amount: number;
+    let sendingAmountCurrencyCode: string;
+    if (!schema.amount.includes(".")) {
+      amount = z.coerce.number().int().parse(schema.amount);
+      sendingAmountCurrencyCode = "SAT";
+    } else if (schema.amount.split(".").length > 2) {
+      throw new Error(
+        "invalid amount string. Cannot contain more than one period. Example: '5000' for SAT or '5.USD' for USD.",
+      );
+    } else {
+      const [amountStr, currencyCode] = schema.amount.split(".");
+      amount = z.coerce.number().int().parse(amountStr);
+      sendingAmountCurrencyCode = currencyCode;
+    }
+
+    return new PayRequest(
+      schema.convert,
+      sendingAmountCurrencyCode,
+      amount,
+      schema.payerData,
+      schema.payeeData,
+    );
+  }
+
+  static fromJson(jsonStr: string): PayRequest {
+    const parsed = JSON.parse(jsonStr);
+    let validated: z.infer<typeof PayRequestSchema>;
+    try {
+      validated = PayRequestSchema.parse(parsed);
+    } catch (e) {
+      throw new Error("invalid pay request", { cause: e });
+    }
+    return this.fromSchema(validated);
+  }
+}
 
 export const RouteSchema = z.object({
   pubkey: z.string(),
@@ -128,6 +216,11 @@ export const RouteSchema = z.object({
 export type Route = z.infer<typeof RouteSchema>;
 
 export const PayReqResponsePaymentInfoSchema = z.object({
+  /**
+   * The amount that the receiver will receive in the receiving currency not including fees. The amount is specified
+   * in the smallest unit of the currency (eg. cents for USD).
+   */
+  amount: z.number(),
   /** currencyCode is the ISO 3-digit currency code that the receiver will receive for this payment. */
   currencyCode: z.string(),
   /**
@@ -138,10 +231,10 @@ export const PayReqResponsePaymentInfoSchema = z.object({
    * details, edge cases, and examples.
    */
   decimals: z.number(),
-  /** multiplier is the conversion rate. It is the number of millisatoshis that the receiver will receive for 1 unit of the specified currency. */
+  /** The conversion rate. It is the number of millisatoshis that the receiver will receive for 1 unit of the specified currency. */
   multiplier: z.number(),
-  /** exchangeFeesMillisatoshi is the fees charged (in millisats) by the receiving VASP for this transaction. This is separate from the Multiplier. */
-  exchangeFeesMillisatoshi: z.number(),
+  /**  The fees charged (in millisats) by the receiving VASP for this transaction. This is separate from the multiplier. */
+  fee: z.number(),
 });
 
 export type PayReqResponsePaymentInfo = z.infer<
@@ -154,7 +247,7 @@ export const PayReqResponseSchema = z.object({
   pr: z.string(),
   /** routes is usually just an empty list from legacy LNURL, which was replaced by route hints in the BOLT11 invoice. */
   routes: optionalIgnoringNull(z.array(RouteSchema)),
-  paymentInfo: PayReqResponsePaymentInfoSchema,
+  converted: PayReqResponsePaymentInfoSchema,
   payeeData: PayeeDataSchema,
 });
 
@@ -200,21 +293,6 @@ export function encodeToUrl(q: LnurlpRequest): URL {
   queryParams.set("umaVersion", q.umaVersion);
   lnurlpUrl.search = queryParams.toString();
   return lnurlpUrl;
-}
-
-export function encodePayRequest(q: PayRequest) {
-  return JSON.stringify(q);
-}
-
-export function parsePayRequest(payRequest: string): PayRequest {
-  const parsed = JSON.parse(payRequest);
-  let validated: PayRequest;
-  try {
-    validated = PayRequestSchema.parse(parsed);
-  } catch (e) {
-    throw new Error("invalid pay request", { cause: e });
-  }
-  return validated;
 }
 
 export function parsePayReqResponse(jsonStr: string): PayReqResponse {
