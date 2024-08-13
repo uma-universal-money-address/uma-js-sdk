d6150fcce61813b6e1c106e6f0d088eba33cf6c8
Brian Siao Tick Chong <bsiaotickchong@gmail.com>
2024-08-12 17:42:08 -0700

    Add button customizability, wrap web component in react component

--- packages/uma-auth-client/src/components/UmaConnectButton.tsx
+++ packages/uma-auth-client/src/components/UmaConnectButton.tsx
@@ -1,26 +1,50 @@
-import { ThemeProvider } from "@emotion/react";
-import { Button } from "@lightsparkdev/ui/components";
-import { themes } from "@lightsparkdev/ui/styles/themes";
-import { GlobalStyles } from "src/GlobalStyles";
+import { css } from "@emotion/css";
+import styled from "@emotion/styled";
+import { Icon, UnstyledButton } from "@lightsparkdev/ui/components";
+import defineWebComponent from "src/utils/defineWebComponent";
 
-export const UmaConnectButton = () => {
+export const TAG_NAME = "uma-connect-button";
+
+const UmaConnectButton = () => {
   const clientId = "1";
-  const redirectUri = "http://localhost:3000";
+  const redirectUri = "http://localhost:3001";
   const responseType = "code";
   const codeChallenge = "1234";
   const codeChallengeMethod = "S256";
 
   return (
-    <ThemeProvider theme={themes.umameDocsLight}>
-      <GlobalStyles />
-      <Button
-        icon="Uma"
-        text="Connect"
-        kind="primary"
-        onClick={() =>
-          (window.location.href = `http://localhost:3000/apps/new?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&code_challenge=${codeChallenge}&code_challenge_method=${codeChallengeMethod}&required_commands=send_payments&optional_commands=read_balance,read_transactions&budget=10.USD%2Fmonthly&expiration_period=year`)
-        }
+    <Button
+      onClick={() =>
+        (window.location.href = `http://localhost:3000/apps/new?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}&code_challenge=${codeChallenge}&code_challenge_method=${codeChallengeMethod}&required_commands=send_payments&optional_commands=read_balance,read_transactions&budget=10.USD%2Fmonthly&expiration_period=year`)
+      }
+    >
+      <Icon
+        name="Uma"
+        width={36}
+        className={css`
+          color: var(--uma-connect-text-color, #ffffff);
+        `}
       />
-    </ThemeProvider>
+      Connect
+    </Button>
   );
 };
+
+const Button = styled(UnstyledButton)`
+  background-color: var(--uma-connect-background, #0068c9);
+  border-radius: var(--uma-connect-radius, 999px);
+  padding-left: var(--uma-connect-padding-x, 32px);
+  padding-right: var(--uma-connect-padding-x, 32px);
+  padding-top: var(--uma-connect-padding-y, 16px);
+  padding-bottom: var(--uma-connect-padding-y, 16px);
+  color: var(--uma-connect-text-color, #ffffff);
+  font-family: var(--uma-connect-font-family, "Manrope");
+  font-size: var(--uma-connect-font-size, 16px);
+  font-weight: var(--uma-connect-font-weight, 700);
+`;
+
+defineWebComponent(TAG_NAME, UmaConnectButton);
+
+export const UmaConnectButtonWebComponent = (props: Record<string, any>) => (
+  <uma-connect-button {...props} />
+);
