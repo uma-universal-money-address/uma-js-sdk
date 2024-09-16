fbd599d9c9ab05a65140ea7b5fad8a893c3cf2a4
Jeremy Klein <jklein24@gmail.com>
2024-09-16 14:28:47 -0700

    lint

--- packages/uma-auth-client/src/types/errors.ts
+++ packages/uma-auth-client/src/types/errors.ts
@@ -1,7 +1,6 @@
 export class UmaUnsupportedError extends Error {
-    constructor(message: string = 'UMA Auth is not supported by this wallet') {
-        super(message);
-        this.name = 'UmaUnsupportedError';
-    }
+  constructor(message: string = "UMA Auth is not supported by this wallet") {
+    super(message);
+    this.name = "UmaUnsupportedError";
+  }
 }
-
