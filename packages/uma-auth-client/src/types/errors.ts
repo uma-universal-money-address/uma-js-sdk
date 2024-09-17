4675cb826f1870ccc3703d2027165e0ffd40cb70
Jeremy Klein <jklein24@gmail.com>
2024-09-16 18:07:09 -0700

    Fix the handling of discovery document errors. (#152)
    * Fix the handling of discovery document errors.
    
    * Update packages/uma-auth-client/src/hooks/useDiscoveryDocument.tsx
    
    Co-authored-by: Brian Siao Tick Chong <bsiaotickchong@gmail.com>
    
    * lint
    
    * Lint again
    
    ---------
    
    Co-authored-by: Brian Siao Tick Chong <bsiaotickchong@gmail.com>

---
+++ packages/uma-auth-client/src/types/errors.ts
@@ -0,0 +1,6 @@
+export class UmaUnsupportedError extends Error {
+  constructor(message: string = "UMA Auth is not supported by this wallet") {
+    super(message);
+    this.name = "UmaUnsupportedError";
+  }
+}
